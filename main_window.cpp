db1b7e7cd0c62c1d6d4d16e2fbd9f283768f4a7e
Mark Geck <mgeck64@gmail.com>
2022-02-24 02:55:47 -0800

    Make CLR button resize to minimum size

--- main_window.cpp
+++ main_window.cpp
@@ -304,9 +304,7 @@ auto main_window::on_clear_result_btn_clicked() -> void {
     expr_entry.set_text(Glib::ustring());
     result_lbl.set_label(Glib::ustring());
     last_result_kind = none_kind;
-
-    set_default_size(-1, -1); // this is actually having no effect but we try
-
+    resize(1, 1); // actually resizes to minimum width/height
     expr_entry.grab_focus_without_selecting();
 }
 
