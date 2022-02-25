84bb374ec58d145859446d4ca82173b227425a32
Mark Geck <mgeck64@gmail.com>
2022-02-25 13:16:05 -0800

    Bump version number

--- gcalc_basics.hpp
+++ gcalc_basics.hpp
@@ -14,7 +14,7 @@ constexpr auto app_name = "gcalc";
 constexpr auto app_name_bold = "<b>gcalc</b>";
 constexpr auto app_id = "com.github.mgeck64.GCalc";
 constexpr auto app_icon = "accessories-calculator";
-constexpr auto app_version = "0.1.0";
+constexpr auto app_version = "0.2.0";
 constexpr auto app_copyright = "Copyright \u00a9 2022 Mark Geck";
 
 inline void set_margin(Gtk::Widget& w, int margin) {
